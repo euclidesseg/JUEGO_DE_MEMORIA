6ad6d4ef5b97d6241013a8dce646942f216387b1
EUCLIDES PEREZ <89555967+euclidesseg@users.noreply.github.com>
2022-05-12 20:54:32 -0500

    Update funciones.js

--- JUEGO-MEMORIA/funciones.js
+++ JUEGO-MEMORIA/funciones.js
@@ -1,4 +1,72 @@
 document.addEventListener("DOMContentLoaded", function(){
+    //declaarar variables
+    let imagenes = [
+        {
+            url:"assets/imagen1.jpg",
+            nombre:"planetas"
+        },
+        {
+            url:"assets/imagen2.jpg",
+            nombre:"planetas"
+        },
+        {
+            url:"assets/imagen3.jpg",
+            nombre:"planetas"
+        },
+        {
+            url:"assets/imagen4.jpg",
+            nombre:"planetas"
+        },
+        {
+            url:"assets/imagen5.jpg",
+            nombre:"planetas"
+        },
+        {
+            url:"assets/imagen6.jpg",
+            nombre:"planetas"
+        },
+        
+        {
+            url:"assets/imagen1.jpg",
+            nombre:"planetas"
+        },
+        {
+            url:"assets/imagen2.jpg",
+            nombre:"planetas"
+        },
+        {
+            url:"assets/imagen3.jpg",
+            nombre:"planetas"
+        },
+        {
+            url:"assets/imagen4.jpg",
+            nombre:"planetas"
+        },
+        {
+            url:"assets/imagen5.jpg",
+            nombre:"planetas"
+        },
+        {
+            url:"assets/imagen6.jpg",
+            nombre:"planetas"
+        }
+        
+    ];
+    //guardar el tablero
+    let tablero = document.querySelector(".tablero");
 
+    //funcion para agregar las imagenes al talero
+    function agregarImagenes(){
+        for(index = 0; index < imagenes.length; index++){
+            let img = document.createElement("img")
+            img.setAttribute("src","assets/imagen8.webp");
+            img.setAttribute("width","200px");
+            img.setAttribute("heigth","200px");
+            // pasar al tablero las imagenes
+
+            tablero.appendChild(img);
+        }
+    }
+    agregarImagenes();
 
 })
